87ed13f11bce0c08ff0538567b9894c97865f9cc
Psycho Mods <nishsin26@gmail.com>
2018-01-21 19:15:12 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Dot stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/psycho/config/common_full_phone.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
